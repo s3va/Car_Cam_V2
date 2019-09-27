4c6e1eac70948c350b6dc8b3e9ecd1b145ccb973
seva <seva@kvakva.tk>
2019-09-28 02:28:24 +0300

    getExternalFilesDir(Environment.DIRECTORY_MOVIES) added

--- app/src/main/java/tk/kvakva/carcamv2/MainActivity.kt
+++ app/src/main/java/tk/kvakva/carcamv2/MainActivity.kt
@@ -1,18 +1,19 @@
 package tk.kvakva.carcamv2
 
 import android.app.Application
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.*
 import androidx.lifecycle.Observer
 import androidx.preference.PreferenceManager
@@ -20,7 +21,9 @@ import com.otaliastudios.cameraview.CameraException
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.CameraOptions
 import com.otaliastudios.cameraview.VideoResult
+import com.otaliastudios.cameraview.controls.Flash
 import kotlinx.android.synthetic.main.activity_main.*
+import tk.kvakva.carcamv2.databinding.ActivityMainBinding
 import java.io.File
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
@@ -31,16 +34,23 @@ const val LOG_TAG = "MAinACtivity"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var camViewModel: CamViewModel
+    //private lateinit var camViewModel: CamViewModel
+
+    private val camViewModel by viewModels<CamViewModel>()
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        camViewModel = ViewModelProvider(this).get(CamViewModel::class.java)
-
+//        camViewModel = ViewModelProvider(this).get(CamViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.maViewModel = camViewModel
+        binding.lifecycleOwner = this
+        binding.txtView.movementMethod=ScrollingMovementMethod()
         camV.setLifecycleOwner(this)
-        camV.videoMaxDuration = 60000
+        //     camV.videoMaxDuration = 60000
 
         camV.addCameraListener(object : CameraListener() {
             override fun onCameraError(exception: CameraException) {
@@ -57,7 +67,9 @@ class MainActivity : AppCompatActivity() {
                     LOG_TAG,
                     "************************ onVideoTaken ************** filename: ${fileN.name}  ----------"
                 )
-                txtView.text = fileN.name
+
+                //txtView.text = fileN.name
+                camViewModel.appViewText("\n${fileN.name}")
                 if (camViewModel.isRecordingVideo.value == true) {
                     camViewModel.setTimeOutRec()
                 }
@@ -83,6 +95,15 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        binding.videoRecordBtn.setOnLongClickListener {
+            Log.i(LOG_TAG,"-- long click pressed ${camV.flash}")
+            camV.flash = when (camV.flash) {
+                Flash.OFF -> Flash.TORCH
+                else -> Flash.OFF
+            }
+            Log.i(LOG_TAG,"-  long click pressed ${camV.flash}")
+            true
+        }
         val mObserver = Observer<Boolean> {
             if (camViewModel.timeOutRec.value == true) {
                 camViewModel.unsetTimeOutRec()
@@ -195,9 +216,85 @@ class MainActivity : AppCompatActivity() {
         }
 
 */
+        val sp = PreferenceManager.getDefaultSharedPreferences(application)
+        val maxNFiles = sp.getString("maxNumberOfVideos", "1000")?.toInt() ?: 1000
+        val minFrSpace = sp.getString("minFreeSpace", "1000000000")?.toLong() ?: 1000000000
+        var flist = getExternalFilesDir(Environment.DIRECTORY_MOVIES)
+            ?.listFiles { _, fname ->
+                fname.endsWith(".carcam.mp4")
+            }?.sorted()
+
+        flist?.forEach {
+            Log.i(
+                LOG_TAG,
+                "---- $it"
+            )
+        }
+        var freespace = getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.freeSpace
+        Log.i(
+            LOG_TAG,
+            "----------------------- PRE while getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.freeSpace = $freespace --- maxNFiles: $maxNFiles --- minFrSpace: $minFrSpace"
+        )
+
+        while (freespace != null && freespace < minFrSpace && (flist?.size ?: 0) > 10) {
+            // 4_356_866_048
+            Log.i(LOG_TAG, "flist?.first()?.delete() *** ${flist?.first()}")
+            flist?.first()?.delete()
+            flist = getExternalFilesDir(Environment.DIRECTORY_MOVIES)
+                ?.listFiles { _, fname ->
+                    fname.endsWith(".carcam.mp4")
+                }?.sorted()
+            freespace = getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.freeSpace
+            Log.i(
+                LOG_TAG,
+                "in while getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.freeSpace = $freespace"
+            )
+        }
+        Log.i(
+            LOG_TAG,
+            "----------------------- AFTER while getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.freeSpace = $freespace"
+        )
+
+        if (flist?.size ?: 0 >= maxNFiles) {
+            for (i in 0..(flist?.size ?: 0) - maxNFiles) {
+                flist?.elementAt(i)?.delete()
+                Log.i(LOG_TAG, "flist?.elementAt(i)?.delete() ${flist?.elementAt(i)}")
+            }
+        }
+        Log.i(
+            LOG_TAG,
+            "----------------------- AFTER for getExternalFilesDir(Environment.DIRECTORY_MOVIES)?.freeSpace = $freespace"
+        )
+        flist = getExternalFilesDir(Environment.DIRECTORY_MOVIES)
+            ?.listFiles { _, fname ->
+                fname.endsWith(".carcam.mp4")
+            }?.sorted()
+
+        flist?.forEach {
+            Log.i(
+                LOG_TAG,
+                "---- $it"
+            )
+        }
+
+        val f = File(getExternalFilesDir(Environment.DIRECTORY_MOVIES), "$sTime.carcam.mp4")
+//        val dirar = getExternalFilesDirs(Environment.DIRECTORY_MOVIES + File.separator + "CarCamV20")
+        //val dirarqq = getExternalFilesDirs(Environment.DIRECTORY_MOVIES + File.separator + "CarCamV20qqq")
+//        Log.i(LOG_TAG,"getExternalFileDirs = ${dirar.toList()}")
+//        val dir = dirar.last()
+//        Log.i(LOG_TAG,"dir = $dir")
+//        val f = File(dir,"$sTime.mp4")
+        Log.i(LOG_TAG, "File f = $f")
+        Log.i(
+            LOG_TAG,
+            "getExternalFilesDir(Environment.DIRECTORY_MOVIES) = ${getExternalFilesDir(
+                Environment.DIRECTORY_MOVIES
+            )}  getExternalFilesDir(Environment.DIRECTORY_MOVIES) = ${getExternalFilesDir(
+                Environment.DIRECTORY_MOVIES
+            )}  ${Environment.DIRECTORY_MOVIES}"
+        )
 
 
-        val f = File(getExternalFilesDir(Environment.DIRECTORY_MOVIES), "$sTime.mp4")
         if (camV.isTakingVideo)
             Log.e(LOG_TAG, "!!!!!! startRecord() !!! camV.isTakeingVideo!!!!!!")
         else
@@ -205,7 +302,8 @@ class MainActivity : AppCompatActivity() {
         camViewModel.startVideo()
         videoRecordBtn.setImageResource(android.R.drawable.star_on)
         Log.v(LOG_TAG, "********* startRecord() f: ${f.name} *****************")
-        txtView.append("\n${f.name}")
+        //txtView.append("\n${f.name}")
+        camViewModel.appViewText("\n${f.name}")
     }
 
     private fun stopRecord() {
@@ -248,6 +346,17 @@ class CamViewModel(aplica: Application) : AndroidViewModel(aplica) {
     private val _duration = MutableLiveData<Int>()
     val duration: LiveData<Int>
         get() = _duration
+
+    private val _textViewText = MutableLiveData<String>("wawaawawaw awawawaw")
+    val textViewText: LiveData<String> = _textViewText
+
+    fun setViewText(s: String) {
+        _textViewText.postValue(s)
+    }
+
+    fun appViewText(s: String) {
+        _textViewText.postValue((textViewText.value + s).takeLast(350))
+    }
 
     init {
         val sp = PreferenceManager.getDefaultSharedPreferences(getApplication())
